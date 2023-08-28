24e54888cb61d2b408aaf3a136afd53dfa65e633
meanuragbharti <101868239+meanuragbharti@users.noreply.github.com>
2023-08-28 23:06:55 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -8,7 +8,27 @@ contract DegenToken is ERC20, Ownable {
 
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    // Mint new tokens
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Redeem tokens for in-game items
+    function redeem(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
+        _burn(msg.sender, amount);
+        // Perform in-game item redemption logic here
+    }
+
+    // Transfer tokens to others
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    // Burn tokens that are no longer needed
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
+        _burn(msg.sender, amount);
     }
 }
